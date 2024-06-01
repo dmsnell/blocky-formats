d90eb546db2c6b757a17acfe0d3a20b32f3454c6
Dennis Snell <dmsnell@xkq.io>
2024-06-01 23:52:57 +0200

    Call logic from Markdown module.

--- src/markdown.js
+++ src/markdown.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /**
  * Convert between Markdown and WordPress Blocks.
  *
@@ -57,7 +59,7 @@ const blockToMarkdown = (state, block) => {
 
     switch (block.name) {
         case 'core/quote':
-            const content = blocksToTrac(state, block.innerBlocks);
+            const content = blocksToMarkdown(state, block.innerBlocks);
             // @todo this probably fails on nested quotes - handle that.
             return content.split(/\n/g).map(l => `> ${l}`).join('\n') + '\n\n';
 
@@ -145,269 +147,264 @@ const blocksToMarkdown = (state, blocks) => {
     return blocks.map(block => blockToMarkdown(state, block)).join('').replace(/^[\n\r]+|[\n\r]+$/g, '');
 }
 
-window.saveToMarkdown = () => {
+export const blocks2markdown = blocks => {
     const state = {
         indent: 0,
         listStyle: [],
     };
 
-    const blocks = wp.data.select('core/block-editor').getBlocks();
-    const markdown = blocksToMarkdown(state, blocks || []);
-    console.log(markdown);
-    return markdown;
+    return blocksToMarkdown(state, blocks || []);
 }
 
-window.loadFromMarkdown = input => {
-    "use strict";
+function WpBlocksRenderer(options) {
+    this.options = options;
+}
 
-    function WpBlocksRenderer(options) {
-        this.options = options;
+const escapeHTML = s => s.replace(/[<&>'"]/g, (m) => {
+    switch (m[0]) {
+        case '<':
+            return '&lt;';
+        case '>':
+            return '&gt;';
+        case '&':
+            return '&amp;';
+        case '"':
+            return '&quot;';
+        case "'":
+            return '&apos;';
+    }
+});
+
+function render(ast) {
+    var blocks = {
+        name: 'root',
+        attributes: {},
+        innerBlocks: [],
+    };
+    var event, lastNode;
+    var walker = ast.walker();
+
+    while ((event = walker.next())) {
+        lastNode = event.node;
     }
 
-    const escapeHTML = s => s.replace(/[<&>'"]/g, (m) => {
-        switch (m[0]) {
-            case '<':
-                return '&lt;';
-            case '>':
-                return '&gt;';
-            case '&':
-                return '&amp;';
-            case '"':
-                return '&quot;';
-            case "'":
-                return '&apos;';
-        }
-    });
-
-    function render(ast) {
-        var blocks = {
-            name: 'root',
-            attributes: {},
-            innerBlocks: [],
-        };
-        var event, lastNode;
-        var walker = ast.walker();
-
-        while ((event = walker.next())) {
-            lastNode = event.node;
-        }
-
-        // Walk the blocks
-        if (lastNode.type !== 'document') {
-            throw new Error('Expected a document node');
-        }
-
-        nodeToBlock(blocks, lastNode.firstChild);
-
-        return blocks.innerBlocks;
+    // Walk the blocks
+    if (lastNode.type !== 'document') {
+        throw new Error('Expected a document node');
     }
 
-    const nodeToBlock = (parentBlock, node) => {
-        const add = block => {
-            parentBlock.innerBlocks.push(block);
+    nodeToBlock(blocks, lastNode.firstChild);
+
+    return blocks.innerBlocks;
+}
+
+const nodeToBlock = (parentBlock, node) => {
+    const add = block => {
+        parentBlock.innerBlocks.push(block);
+    }
+
+    const block = {
+        name: '',
+        attributes: {},
+        innerBlocks: [],
+    };
+
+    let skipChildren = false;
+
+    /**
+     * @see ../blocks.js
+     */
+    switch (node.type || null) {
+        // Nothing to store here. It's a container.
+        case 'document':
+            // @todo Should this "break" instead?
+            return;
+
+        case 'image':
+            // @todo If there's formatting, grab it from the children.
+            block.name = 'core/image';
+            block.attributes.url = node._destination;
+            if (node._description) {
+                block.attributes.alt = node._description
+            }
+            if (node._title) {
+                block.attributes.title = node._title;
+            }
+            break;
+
+        case 'list':
+            block.name = 'core/list';
+            block.attributes.ordered = node._listData.type === 'ordered';
+            if (node._listData.start && node._listData.start !== 1) {
+                block.attributes.start = node._listData.start;
+            }
+            break;
+
+        case 'block_quote':
+            block.name = 'core/quote';
+            break;
+
+        case 'item': {
+            // @todo WordPress' list block doesn't support inner blocks.
+            block.name = 'core/list-item';
+            // There's a paragraph wrapping the list content.
+            let innerNode = node.firstChild;
+            while (innerNode) {
+                switch (innerNode.type) {
+                    case 'paragraph':
+                        block.attributes.content = inlineBlocksToHTML('', innerNode.firstChild);
+                        break;
+
+                    case 'list':
+                        nodeToBlock(block, innerNode);
+                        break;
+
+                    default:
+                        console.log(innerNode);
+                }
+
+                innerNode = innerNode.next;
+            }
+            skipChildren = true;
+            break;
         }
 
-        const block = {
-            name: '',
-            attributes: {},
-            innerBlocks: [],
-        };
+        case 'heading':
+            block.name = 'core/heading';
+            // Content forms nodes starting with .firstChild -> .next -> .next
+            block.attributes.level = node.level;
+            block.attributes.content = inlineBlocksToHTML('', node.firstChild);
+            skipChildren = true;
+            break;
 
-        let skipChildren = false;
+        case 'thematic_break':
+            block.name = 'core/separator';
+            break;
 
-        /**
-         * @see ../blocks.js
-         */
-        switch (node.type || null) {
-            // Nothing to store here. It's a container.
-            case 'document':
-                // @todo Should this "break" instead?
-                return;
+        case 'code_block':
+            block.name = 'core/code';
+            if ('string' === typeof node.info && '' !== node.info) {
+                block.attributes.language = node.info.replace(/[ \t\r\n\f].*/, '');
+            }
+            block.attributes.content = node.literal.replace(/\n/g, '<br>');
+            break;
 
-            case 'image':
+        case 'html_block':
+            block.name = 'core/html';
+            block.attributes.content = node.literal;
+            break;
+
+        case 'paragraph':
+            // @todo Handle inline HTML, which should be an HTML block.
+            if (node.firstChild && node.firstChild.type === 'image' && !node.firstChild.next) {
                 // @todo If there's formatting, grab it from the children.
+                const image = node.firstChild;
                 block.name = 'core/image';
-                block.attributes.url = node._destination;
-                if (node._description) {
-                    block.attributes.alt = node._description
+                block.attributes.url = image._destination;
+                if (image._title && '' !== image._title) {
+                    block.attributes.caption = image._title;
+                } else if (image.firstChild) {
+                    block.attributes.caption = inlineBlocksToHTML('', image.firstChild);
                 }
-                if (node._title) {
-                    block.attributes.title = node._title;
-                }
-                break;
-
-            case 'list':
-                block.name = 'core/list';
-                block.attributes.ordered = node._listData.type === 'ordered';
-                if (node._listData.start && node._listData.start !== 1) {
-                    block.attributes.start = node._listData.start;
-                }
-                break;
-
-            case 'block_quote':
-                block.name = 'core/quote';
-                break;
-
-            case 'item': {
-                // @todo WordPress' list block doesn't support inner blocks.
-                block.name = 'core/list-item';
-                // There's a paragraph wrapping the list content.
-                let innerNode = node.firstChild;
-                while (innerNode) {
-                    switch (innerNode.type) {
-                        case 'paragraph':
-                            block.attributes.content = inlineBlocksToHTML('', innerNode.firstChild);
-                            break;
-
-                        case 'list':
-                            nodeToBlock(block, innerNode);
-                            break;
-
-                        default:
-                            console.log(innerNode);
-                    }
-
-                    innerNode = innerNode.next;
+                if (image._description && '' !== image._description) {
+                    block.attributes.alt = image._description;
                 }
                 skipChildren = true;
                 break;
             }
 
-            case 'heading':
-                block.name = 'core/heading';
-                // Content forms nodes starting with .firstChild -> .next -> .next
-                block.attributes.level = node.level;
-                block.attributes.content = inlineBlocksToHTML('', node.firstChild);
-                skipChildren = true;
-                break;
+            block.name = 'core/paragraph';
+            block.attributes.content = inlineBlocksToHTML('', node.firstChild);
+            skipChildren = true;
+            break;
 
-            case 'thematic_break':
-                block.name = 'core/separator';
-                break;
-
-            case 'code_block':
-                block.name = 'core/code';
-                if ('string' === typeof node.info && '' !== node.info) {
-                    block.attributes.language = node.info.replace(/[ \t\r\n\f].*/, '');
-                }
-                block.attributes.content = node.literal.replace(/\n/g, '<br>');
-                break;
-
-            case 'html_block':
-                block.name = 'core/html';
-                block.attributes.content = node.literal;
-                break;
-
-            case 'paragraph':
-                // @todo Handle inline HTML, which should be an HTML block.
-                if (node.firstChild && node.firstChild.type === 'image' && !node.firstChild.next) {
-                    // @todo If there's formatting, grab it from the children.
-                    const image = node.firstChild;
-                    block.name = 'core/image';
-                    block.attributes.url = image._destination;
-                    if (image._title && '' !== image._title) {
-                        block.attributes.caption = image._title;
-                    } else if (image.firstChild) {
-                        block.attributes.caption = inlineBlocksToHTML('', image.firstChild);
-                    }
-                    if (image._description && '' !== image._description) {
-                        block.attributes.alt = image._description;
-                    }
-                    skipChildren = true;
-                    break;
-                }
-
-                block.name = 'core/paragraph';
-                block.attributes.content = inlineBlocksToHTML('', node.firstChild);
-                skipChildren = true;
-                break;
-
-            default:
-                console.log(node);
-        }
-
-        add(block);
-
-        if (!skipChildren && node.firstChild) {
-            nodeToBlock(block, node.firstChild);
-        }
-
-        if (node.next) {
-            nodeToBlock(parentBlock, node.next);
-        }
+        default:
+            console.log(node);
     }
 
-    const inlineBlocksToHTML = (html, node) => {
-        if (!node) {
-            return html;
-        }
+    add(block);
 
-        const add = s => html += s;
-        const surround = (before, after) =>
-            add(
-                before +
-                inlineBlocksToHTML('', node.firstChild) +
-                after
-            );
+    if (!skipChildren && node.firstChild) {
+        nodeToBlock(block, node.firstChild);
+    }
 
-        const addTag = (tag, tagAttrs) => {
-            const attrs = tagAttrs
-                ? (' ' + Object.entries(tagAttrs).filter(([, value]) => value !== null).map(([name, value]) => `${name}="${value}"`).join(' '))
-                : '';
-            const isVoid = 'img' === tag;
-            surround(`<${tag}${attrs}>`, isVoid ? '' : `</${tag}>`);
-        };
+    if (node.next) {
+        nodeToBlock(parentBlock, node.next);
+    }
+}
 
-        switch (node.type) {
-            case 'code':
-                add(`<code>${escapeHTML(node.literal)}</code>`);
-                break;
-
-            case 'emph':
-                addTag('em');
-                break;
-
-            case 'html_inline':
-                add(escapeHTML(node.literal));
-                break;
-
-            case 'image':
-                // @todo If there's formatting, grab it from the children.
-                addTag('img', {src: node._destination, title: node._title || null, alt: node._description || null});
-                break;
-
-            case 'link':
-                addTag('a', {href: node._destination, title: node._title || null});
-                break;
-
-            case 'softbreak':
-                add('<br>');
-                break;
-
-            case 'strong':
-                addTag('strong');
-                break;
-
-            case 'text':
-                add(node.literal);
-                break;
-
-            default:
-                console.log(node);
-        }
-
-        if (node.next) {
-            return inlineBlocksToHTML(html, node.next);
-        }
-
+const inlineBlocksToHTML = (html, node) => {
+    if (!node) {
         return html;
     }
 
-    WpBlocksRenderer.prototype = Object.create(commonmark.Renderer.prototype);
+    const add = s => html += s;
+    const surround = (before, after) =>
+        add(
+            before +
+            inlineBlocksToHTML('', node.firstChild) +
+            after
+        );
 
-    WpBlocksRenderer.prototype.render = render;
-    WpBlocksRenderer.prototype.esc = s => s;
+    const addTag = (tag, tagAttrs) => {
+        const attrs = tagAttrs
+            ? (' ' + Object.entries(tagAttrs).filter(([, value]) => value !== null).map(([name, value]) => `${name}="${value}"`).join(' '))
+            : '';
+        const isVoid = 'img' === tag;
+        surround(`<${tag}${attrs}>`, isVoid ? '' : `</${tag}>`);
+    };
 
+    switch (node.type) {
+        case 'code':
+            add(`<code>${escapeHTML(node.literal)}</code>`);
+            break;
+
+        case 'emph':
+            addTag('em');
+            break;
+
+        case 'html_inline':
+            add(escapeHTML(node.literal));
+            break;
+
+        case 'image':
+            // @todo If there's formatting, grab it from the children.
+            addTag('img', {src: node._destination, title: node._title || null, alt: node._description || null});
+            break;
+
+        case 'link':
+            addTag('a', {href: node._destination, title: node._title || null});
+            break;
+
+        case 'softbreak':
+            add('<br>');
+            break;
+
+        case 'strong':
+            addTag('strong');
+            break;
+
+        case 'text':
+            add(node.literal);
+            break;
+
+        default:
+            console.log(node);
+    }
+
+    if (node.next) {
+        return inlineBlocksToHTML(html, node.next);
+    }
+
+    return html;
+}
+
+WpBlocksRenderer.prototype = Object.create(commonmark.Renderer.prototype);
+
+WpBlocksRenderer.prototype.render = render;
+WpBlocksRenderer.prototype.esc = s => s;
+
+export const markdownToBlocks = input => {
     const frontMatterMatch = frontMatterPattern.exec(input);
     const foundFrontMatter = null !== frontMatterMatch;
     const frontMatter = foundFrontMatter ? frontMatterMatch[1] : null;
